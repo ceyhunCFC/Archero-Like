e777059a40fcd56e32e1961048a06130d59da774
Ceyhun CIFCI <ceyhun-cfc@hotmail.com>
2025-02-21 20:16:00 +0300

    Joystick

---
+++ Assets/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FloatingJoystick : Joystick
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        background.anchoredPosition = new Vector2(0,-700);
+
+        base.OnPointerUp(eventData);
+    }
+}
